d4ab40fc733e1cf6413c559de923faec8afb228a
dnbucklin <david.bucklin@gmail.com>
2015-07-24 12:40:38 -0400

    Update pgis2slin.r

--- pgis2slin.r
+++ pgis2slin.r
@@ -1,17 +1,14 @@
-# Function creates a new SpatialLines or SpatialLinesDataFrame object from a table with geometry in Postgresql. 
-# Requires that a connection already is established to the database using the dbDriver() and dbConnect() functions in the package RPostgreSQL.
-
 pgis2slin <- function(conn,table,geom='geom',gid='gid',proj=NULL,other.cols = '*',query=NULL) {
-
+  
   require(sp)
   require(rgdal)
   require(rgeos)
   
   if (is.null(other.cols))
-    {dfTemp<-dbGetQuery(conn,paste0("select ",gid," as tgid,st_astext(",geom,") as wkt from ",table," where ",geom," is not null ",query,";"))
-    row.names(dfTemp) = dfTemp$tgid}
-  else {dfTemp<-dbGetQuery(conn,paste0("select ",gid," as tgid,st_astext(",geom,") as wkt,",other.cols," from ",table," where ",geom," is not null ",query,";"))
-    row.names(dfTemp) = dfTemp$tgid}
+  {dfTemp<-suppressWarnings(dbGetQuery(conn,paste0("select ",gid," as tgid,st_astext(",geom,") as wkt from ",table," where ",geom," is not null ",query,";")))
+  row.names(dfTemp) = dfTemp$tgid}
+  else {dfTemp<-suppressWarnings(dbGetQuery(conn,paste0("select ",gid," as tgid,st_astext(",geom,") as wkt,",other.cols," from ",table," where ",geom," is not null ",query,";")))
+  row.names(dfTemp) = dfTemp$tgid}
   
   if (is.null(proj)){
     tt<-mapply(function(x,y) readWKT(x,y), x=dfTemp[,2], y=dfTemp[,1])}
@@ -28,10 +25,10 @@ pgis2slin <- function(conn,table,geom='geom',gid='gid',proj=NULL,other.cols = '*
   }))
   
   Sline@proj4string<-slot(tt[[1]], "proj4string")
-
+  
   if (is.null(other.cols)){ return(Sline) } 
   else {try(dfTemp[geom]<-NULL)
-        try(dfTemp['wkt']<-NULL)
+    try(dfTemp['wkt']<-NULL)
     spdf<-SpatialLinesDataFrame(Sline, dfTemp)
     spdf@data['tgid']<-NULL
     return(spdf)}
